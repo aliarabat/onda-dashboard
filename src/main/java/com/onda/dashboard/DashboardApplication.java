48f56d3180be0ba2f943e4d0c69ada38f136f727
Ali <arabat50@gmail.com>
2019-05-30 01:36:20 +0000

    Added eureka dependecy

--- src/main/java/com/onda/dashboard/DashboardApplication.java
+++ src/main/java/com/onda/dashboard/DashboardApplication.java
@@ -2,8 +2,10 @@ package com.onda.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DashboardApplication {
 
     public static void main(String[] args) {
