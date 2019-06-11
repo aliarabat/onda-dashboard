17291b187670b155866e1ce22457410674ba79f5
Elattar Saad <elattar.saad.3@gmail.com>
2019-06-11 02:46:57 +0000

    first commit

--- src/main/java/com/onda/dashboard/DashboardApplication.java
+++ src/main/java/com/onda/dashboard/DashboardApplication.java
@@ -2,10 +2,8 @@ package com.onda.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class DashboardApplication {
 
     public static void main(String[] args) {
